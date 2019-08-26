7b3a17f83169f5a668a0d91769038c410ae43bde
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-26 09:32:59 -0700

    Add example showing that an inherited method can have increased access

---
+++ increase-access-of-inherited-method.java
@@ -0,0 +1,33 @@
+
+/*
+ * From p. 85
+ *
+ * """
+ * A subclass can change the access of a superclass's methods, but only
+ * to provide more access.
+ * """
+ *
+ * I thought there might be a way to do this without explicity overriding
+ * the superclass method, but I think this is what the author meant.
+ *
+ */
+
+class Base {
+    protected void print() {
+        System.out.println("Hello World!");
+    }
+}
+
+class Derived extends Base {
+    public void print() {
+        super.print();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // new Base().print();
+        new Derived().print();
+    }
+}
+
