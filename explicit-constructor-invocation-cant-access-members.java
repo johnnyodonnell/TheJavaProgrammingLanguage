bd8a3854ecc6ed437f4b50777eeb97fbef672777
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-25 20:48:10 -0700

    Add example showing that explicit constructor invocation can't access instance members

---
+++ explicit-constructor-invocation-cant-access-members.java
@@ -0,0 +1,36 @@
+
+/*
+ * From p. 82
+ *
+ * """
+ * Any expressions evaluated as part of an explicit constructor invocation
+ * are not permitted to refer to any of the members of the current
+ * object.
+ * """
+ *
+ * This code fails at a compile time. Here is the message I received
+ * from the compiler:
+ * """
+ * error: cannot reference number before supertype constructor has been called
+ * """
+ *
+ */
+
+class MyClass {
+    private int number = 10;
+
+    MyClass() {
+        this(number);
+    }
+
+    MyClass(int number) {
+        this.number = number;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        new MyClass();
+    }
+}
+
