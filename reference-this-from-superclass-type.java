e00c67af081971788da354b8ec9856bcdaf0b8e2
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-08-27 09:04:41 -0700

    Add example showing that `this` can be set equal to a reference type of the object's superclass

---
+++ reference-this-from-superclass-type.java
@@ -0,0 +1,30 @@
+
+/*
+ * From p. 89
+ *
+ * I wrote out this example because I found it odd that the author
+ * used a cast operation to set `this` equal to a reference type
+ * of that object's superclass. As shown below, this isn't needed
+ * and I'm not sure why it's used in the book.
+ *
+ */
+
+class Base {
+    public String message = "Hi from Base.";
+}
+
+class Derived extends Base {
+    public String message = "Hi from Derived.";
+
+    void print() {
+        Base base = this;
+        System.out.println(base.message);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        new Derived().print();
+    }
+}
+
