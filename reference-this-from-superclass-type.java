96eb99f6c971536807897eef3c53344eabc3adca
Johnny O'Donnell <odjohnny@amazon.com>
2019-08-28 07:11:00 -0700

    Add note to example of refercing `this` from superclass type

--- reference-this-from-superclass-type.java
+++ reference-this-from-superclass-type.java
@@ -7,6 +7,13 @@
  * of that object's superclass. As shown below, this isn't needed
  * and I'm not sure why it's used in the book.
  *
+ * From p. 92
+ *
+ * """
+ * This cast was unnecessary but emphasized that we really wanted
+ * the current object to be treated as an instance of its superclass.
+ * """
+ *
  */
 
 class Base {
