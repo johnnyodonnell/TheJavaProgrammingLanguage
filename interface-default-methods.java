ab2d8501cc3baa53e9beb02acc79d3f6c62b7d6d
Johnny O'Donnell <odjohnny@amazon.com>
2019-08-29 21:42:06 -0700

    Add notes to default methods example

--- interface-default-methods.java
+++ interface-default-methods.java
@@ -1,11 +1,32 @@
 
 /*
- * This isn't covered in the book, but I do know that interfaces
- * can declare method implementations if they are modified
+ * From p. 131
+ *
+ * """
+ * An abstract class can have a partial implementation, protected
+ * parts, static methods, and so on, whereas interfacse are limited
+ * to public constants and public methods with no implementation.
+ * """
+ *
+ * This isn't true anymore. Not sure in which version this changed,
+ * interfaces have much more capability now.
+ *
+ * The `default` modifier isn't covered in the book, but I do know that
+ * interfaces can declare method implementations if they are modified
  * with the `default` modifier.
  *
  * It looks like the `default` modifier acts somewhat like the
  * `protected` modifier
+ *
+ * In conclusion, I still think that implemenation in interfaces
+ * should be avoided. The ability to add implementation in interfaces
+ * was due to the need to provide backwards compatibility. Thus,
+ * this is probably the only reason that would justify providing
+ * implementation in an interface. Also, the book seems to suggest
+ * that interfaces were introduced in order to provide multiple
+ * inheritance without worrying about the problems of multiple
+ * implementation inheritance. Adding implemenation to interfaces
+ * resurfaces the problem of multiple implementation inheritance.
  *
  */
 
