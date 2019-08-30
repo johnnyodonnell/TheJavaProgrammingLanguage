a8889c06a2072686dad2e1c99bc0b88f2120d414
Johnny O'Donnell <odjohnny@amazon.com>
2019-08-29 21:31:49 -0700

    Add information about how this example relates to the book

--- interface-static-method.java
+++ interface-static-method.java
@@ -1,4 +1,12 @@
 
+/*
+ * From p. 131
+ *
+ * In Java 5 I don't belive that this was possible. However, somewhere
+ * along the way this became possible.
+ *
+ */
+
 interface Printer {
     public static void print() {
         System.out.println("Hello World!");
