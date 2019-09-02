d51b52a7f3822d967efbb98c80c3eec87bd7babb
Johnny O'Donnell <odjohnny@amazon.com>
2019-09-01 20:48:35 -0700

    Add additional reference to the book

--- confusing-unicode-methods.java
+++ confusing-unicode-methods.java
@@ -2,6 +2,14 @@
 /*
  * From p. 162
  *
+ * From p. 165
+ *
+ * """
+ * Characters that look the same, or nearly the same, can be confused.
+ * For example, the Latin capital letter n "N" and the Greek capital
+ * letter v "Ν" look alike but are different characters
+ * """
+ *
  * Java supports Unicode for source code characters. However, some
  * Unicode characters look strikingly similar. The o's in the first
  * method are different than the o's in the second method
