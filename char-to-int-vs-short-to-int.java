9136ce915b9bafc143effe8fb3ee24f3aa2759de
Johnny O'Donnell <odjohnny@amazon.com>
2019-09-03 20:59:02 -0700

    Add comment about why a `short` variable can't be set to 0xffff even though it has enough bits to hold that value

--- char-to-int-vs-short-to-int.java
+++ char-to-int-vs-short-to-int.java
@@ -15,6 +15,11 @@ class Main {
         char ch = '\uffff';
         System.out.println("Result: " + (ch + 0));
 
+        // Ideally we could set a short like in the commented out code
+        // below; however, 0xffff is read as the int 65535, instead
+        // of the value -1. -1 can fit in a short, but 65535 cannot
+        // and thus a compiler-time error is thrown.
+        // short sh = 0xffff;
         short sh = (short) ch;
         System.out.println("Result: " + (sh + 0));
     }
