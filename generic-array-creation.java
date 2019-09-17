f7036065c62534a9b97e8aa49bab87eb81067e66
Johnny O'Donnell <odjohnny@amazon.com>
2019-09-16 18:18:43 -0700

    Add note on generic array creation

--- generic-array-creation.java
+++ generic-array-creation.java
@@ -14,6 +14,19 @@
  * https://stackoverflow.com/a/2927427/5832619
  * https://softwareengineering.stackexchange.com/questions/181439/why-cant-java-generics-be-in-arrays
  *
+ * From p. 268
+ *
+ * """
+ * You cannot create an array whose element type is a parameterized type
+ * """
+ *
+ * Further explanation:
+ * https://docs.oracle.com/javase/tutorial/java/generics/restrictions.html#createArrays
+ *
+ * In this explanation, it's interesting that a `List<List<?>>` would
+ * suffer from the same problem. So maybe arrays have a more strict
+ * type contract than reference type objects.
+ *
  */
 
 class Generic<T> {
