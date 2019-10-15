598ab60a0bc936286cd8da5c13ace94dc05ae711
Johnny O'Donnell <odjohnny@amazon.com>
2019-10-14 20:19:22 -0700

    Add example showing an infinitely deep class

---
+++ infinte-class.java
@@ -0,0 +1,25 @@
+
+/*
+ * From p. 391
+ *
+ * """
+ * there are no attempts to prevent a similar infinite recursion with
+ * class types.
+ * """
+ *
+ * `main` results in an infinite loop
+ *
+ */
+
+class Main {
+    private static final Main field = new Main();
+
+    public static void main(String[] args) {
+        Main main = field;
+        while (main != null) {
+            System.out.println("Accessing main's field.");
+            main = main.field;
+        }
+    }
+}
+
